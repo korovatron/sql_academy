a202539f3d22b47a3af9cac9d714f46268d7a518
Neil <neil@logan-5.co.uk>
2025-10-09 17:31:15 +0100

    Bumped cache version

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for SQL Academy PWA
 // Version 1.0 - Cache-first strategy with offline support
 
-const CACHE_NAME = 'sql-academy-v1.3';
+const CACHE_NAME = 'sql-academy-v1.4';
 const urlsToCache = [
   './',
   './index.html',
